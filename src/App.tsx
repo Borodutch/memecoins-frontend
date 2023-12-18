80332e1202c3bfeca292282d7db421940cd86ad7
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-12-18 15:05:24 -0800

    add terms

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { createHashHistory } from 'history'
 import Home from 'pages/Home'
+import Link from 'components/Link'
 import Mint from 'pages/Mint'
 import Navbar from 'components/Navbar'
 import NotFound from 'pages/NotFound'
 import Router, { CustomHistory, Route } from 'preact-router'
+import Terms from 'pages/Terms'
 import Wallet from 'components/Wallet'
 
 export default function () {
@@ -13,9 +15,16 @@ export default function () {
         <Navbar />
         <Router history={createHashHistory() as unknown as CustomHistory}>
           <Route path="/" component={Home} />
+          <Route path="/terms" component={Terms} />
           <Route path="/:chainId/:address" component={Mint} />
           <Route default component={NotFound} />
         </Router>
+        <hr />
+        <span className="opacity-50">
+          <Link url="/#/terms">Terms of service</Link>. Build by{' '}
+          <Link url="https://warpcast.com/borodutch">@borodutch</Link> (consider
+          a follow!)
+        </span>
       </Wallet>
     </div>
   )
---
+++ src/pages/Terms.tsx
@@ -0,0 +1,19 @@
+export default function () {
+  return (
+    <div>
+      <h2>Terms of service</h2>
+      <ul>
+        <li>
+          No one is liable for any losses or ill-doings but the person who
+          deployed the Memecoin contract.
+        </li>
+        <li>
+          Everything on Web3 has a fee. For instance, OpenSea charges 2.5% fee.
+          Here you pay $20 (hey, $20 is $20) to deploy a contract and then 2.5%
+          of the mints to the service.
+        </li>
+        <li>All ERC20 contracts here are upgradeable. Upgrade away!</li>
+      </ul>
+    </div>
+  )
+}
